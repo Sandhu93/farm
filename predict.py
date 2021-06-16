ef1884912002451452173d6dfe121867a2d5e73c
Sandeep B Kadam <sandeep.bkadam9@gmail.com>
2021-06-16 20:22:11 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -37,9 +37,26 @@ gnb
 
 
 # In[16]:
+import Adafruit_DHT
+sensor = Adafruit_DHT.DHT11
+DHT11_pin = 21
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
+if humidity is not None and temperature is not None:
+  print('Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+  print('Failed to get reading from the sensor. Try again!')
+
+temperature = round(temperature)
+l = ['alapuzha','ernakulam','idukki','kannur','kasargod','kollam','kottayam','kozhikode','malappuram','palakkad','pathanamthitta','tiruvananthapuram','trisuur','wayanad']
+val = input("Enter your district: ")
+print(val)
+
+d = dict([(y,x+1) for x,y in enumerate(set(l))])
 
 
-X = [0,4, 3252.4,6.1, 25,0]
+
+X = [0,4, 3252.4,6.1, temperature,0]
 
 arr = np.array(X)
 newarr = arr.reshape(1, -1)
@@ -54,7 +71,7 @@ y_pred = gnb.predict(newarr)
 # In[18]:
 
 
-y_pred
+print(y_pred)
 
 
 # In[ ]:
